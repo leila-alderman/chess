d3b5e1e41c4a25e60919613906ce72124f6cdf44
leila-alderman <leila.alderman@gmail.com>
2019-02-11 18:13:26 -0500

    Refactor Pawn class

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -13,70 +13,97 @@ class Pawn
     #   2) two squares forward on its first move;
     #   3) up_left or up_right to capture.
     if color == "white"
-      possible_moves = list_white_moves(current_position)
+      return list_white_moves(current_position)
     elsif color == "black"
-      possible_moves = list_black_moves(current_position)
+      return list_black_moves(current_position)
     end
-    
-    # No piece can move off of the board.
-    move_list = possible_moves.select{ |square| !square.nil? }
-    # A pawn cannot move to a square that is already occupied by a piece of the same color.
-    move_list = move_list.select{ |square| square.piece.nil? || square.piece.color != color}
   end
 
   private
   def list_white_moves(current)
     move_list = []
+    move_list.push(move_up_one(current)) unless move_up_one(current).nil?
+    move_list.push(move_up_two(current)) unless move_up_two(current).nil?
+    move_list.push(capture_up_left(current)) unless capture_up_left(current).nil?
+    move_list.push(capture_up_right(current)) unless capture_up_right(current).nil? 
+    return move_list
+  end
+
+  def move_up_one(current)
     unless current.up.nil?
       if current.up.piece.nil?
-        move_list.push(current.up)
+        return current.up
       end
     end
-    if current.row == 7
-      move_list.push(current.up.up)
+  end
+
+  def move_up_two(current)
+    if current.row == 7 && current.up.up.piece.nil?
+      return current.up.up
     end
+  end
+
+  def capture_up_left(current)
     unless current.up_left.nil?
       unless current.up_left.piece.nil?
         if current.up_left.piece.color != color
-          move_list.push(current.up_left)
+          return current.up_left
         end
       end
     end
+  end
+
+  def capture_up_right(current)
     unless current.up_right.nil?
       unless current.up_right.piece.nil?
         if current.up_right.piece.color != color
-          move_list.push(current.up_right)
+          return current.up_right
         end
       end
     end
-    return move_list
   end
 
   def list_black_moves(current)
     move_list = []
+    move_list.push(move_down_one(current)) unless move_down_one(current).nil?
+    move_list.push(move_down_two(current)) unless move_down_two(current).nil?
+    move_list.push(capture_dwn_left(current)) unless capture_dwn_left(current).nil?
+    move_list.push(capture_dwn_right(current)) unless capture_dwn_right(current).nil?
+    return move_list
+  end
+
+  def move_down_one(current)
     unless current.down.nil?
       if current.down.piece.nil?
-        move_list.push(current.down)
+        return current.down
       end
     end
-    if current.row == 2
-      move_list.push(current.down.down)
+  end
+
+  def move_down_two(current)
+    if current.row == 2 && current.down.down.piece.nil?
+      return current.down.down
     end
+  end
+
+  def capture_dwn_left(current)
     unless current.dwn_left.nil?
       unless current.dwn_left.piece.nil?
         if current.dwn_left.piece.color != color
-          move_list.push(current.dwn_left)
+          return current.dwn_left
         end
       end
     end
+  end
+
+  def capture_dwn_right(current)
     unless current.dwn_right.nil?
       unless current.dwn_right.piece.nil?
         if current.dwn_right.piece.color != color
-          move_list.push(current.dwn_right)
+          return current.dwn_right
         end
       end
     end
-    return move_list
   end
 
 end
