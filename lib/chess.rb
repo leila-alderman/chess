a06edc7ac81e885819030b7626a19ed3c244290e
leila-alderman <leila.alderman@gmail.com>
2019-02-14 10:49:35 -0500

    Add Chess#show_board_black

--- lib/chess.rb
+++ lib/chess.rb
@@ -16,20 +16,22 @@ class Chess
 
   def play
     system("clear")
-    show_board
+    show_board_white
+    show_board_black
     display_rules
 
   end
 
   private
-  def show_board
+  def show_board_white
     puts "\n\n"
+    print "  "
     for i in 1..board.columns
       print " #{(i + 96).chr} "
     end
     print "\n"
     board.grid.each_with_index do |row, j|
-      print 9-j
+      print "#{8-j} "
       if j % 2 == 1
         row.each_with_index do |square, x|
           if x % 2 == 1
@@ -63,11 +65,65 @@ class Chess
           end
         end
       end
-      print 9-j
+      print " #{8-j}"
       print "\n"
     end
+    print "  "
     for i in 1..board.columns
       print " #{(i + 96).chr} "
+    end
+  end
+
+  def show_board_black
+    puts "\n\n"
+    print "  "
+    for i in 1..board.columns
+      print " #{((9-i) + 96).chr} "
+    end
+    print "\n"
+    reversed_board = board.grid.reverse
+    reversed_board.each_with_index do |row, j|
+      print "#{8-j} "
+      reversed_row = row.reverse
+      if j % 2 == 1
+        reversed_row.each_with_index do |square, x|
+          if x % 2 == 1
+            if square.piece == nil
+              print white("   ")
+            else
+              print white(" #{square.piece.symbol} ")
+            end
+          else
+            if square.piece == nil
+              print gray("   ")
+            else
+              print gray(" #{square.piece.symbol} ")
+            end
+          end
+        end
+      else
+        reversed_row.each_with_index do |square, x|
+          if x % 2 == 0
+            if square.piece == nil
+              print white("   ")
+            else
+              print white(" #{square.piece.symbol} ")
+            end
+          else
+            if square.piece == nil
+              print gray("   ")
+            else
+              print gray(" #{square.piece.symbol} ")
+            end
+          end
+        end
+      end
+      print " #{8-j}"
+      print "\n"
+    end
+    print "  "
+    for i in 1..board.columns
+      print " #{((9-i) + 96).chr} "
     end
   end
 
@@ -75,8 +131,13 @@ class Chess
     "#{color_code}#{text}\e[0m"
   end
   
-  def gray(text); colorize(text, "\e[1;30;47m"); end
-  def white(text); colorize(text, "\e[1;30;107m"); end
+  def gray(text)
+    colorize(text, "\e[1;30;47m")
+  end
+  
+  def white(text)
+    colorize(text, "\e[1;30;107m")
+  end
 
   def display_rules
   end
